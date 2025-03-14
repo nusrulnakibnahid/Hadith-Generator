7e6a9b1f8df29fe8f29096320102ae7da1de6c78
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2025-03-15 02:58:38 +0600

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,7 +116,7 @@ const translateToBangla = async (text) => {
         banglaElement.textContent = `বাংলা অনুবাদ: ${data.translatedText}`;
     } catch (error) {
         console.error("Error translating Hadith:", error);
-        banglaElement.textContent = "বাংলা অনুবাদ: শিগগিরই উপলব্ধ হবে। (Coming soon)";
+        banglaElement.textContent = "বাংলা অনুবাদ: শিগগিরই উপলব্ধ হবে...। (Coming soon...)";
     }
 };
 
